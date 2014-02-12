a0120eb397871191116385fe35f04a94a448851c
hdunderscore <hdunderscore@gmail.com>
2014-02-12 14:31:35 +1100

    Created repo

---
+++ Lux/fs.glsl
@@ -0,0 +1,286 @@
+//////////////////////////////// Fragment shader
+#version 120
+
+varying vec3 iFS_Normal;
+varying vec2 iFS_UV;
+varying vec3 iFS_Tangent;
+varying vec3 iFS_Binormal;
+varying vec3 iFS_PointWS;
+
+uniform vec3 Lamp0Pos = vec3(0.0f,0.0f,70.0f);
+uniform vec3 Lamp0Color = vec3(1.0f,1.0f,1.0f);
+uniform float Lamp0Intensity = 1.0f;
+uniform vec3 Lamp1Pos = vec3(70.0f,0.0f,0.0f);
+uniform vec3 Lamp1Color = vec3(0.198f,0.198f,0.198f);
+uniform float Lamp1Intensity = 1.0f;
+
+uniform float Kr = 0.5f;
+uniform vec3 ambientColor = vec3(0.07f,0.07f,0.07f);
+uniform float tiling = 1.0f;
+
+uniform bool flipY = true;
+uniform float heightMapScale = 3.0f;
+
+uniform bool LUX_AO = true;
+uniform bool LUX_AO_TILE = false;
+uniform bool LUX_LINEAR = false;
+uniform float HDR_Scale = 6.0f;
+uniform bool DiffHDR = false;
+uniform bool SpecHDR = false;
+uniform float DiffuseExposure=1.0f;
+uniform float SpecularExposure=2.0f;
+uniform float DiffuseLOD = 5.0f;
+
+uniform float Lamp0Power=2.0f;
+uniform float Lamp1Power=1.0f;
+
+uniform sampler2D normalMap;
+uniform sampler2D diffuseMap;
+uniform sampler2D specularMap;
+uniform sampler2D emissiveMap;
+uniform samplerCube environmentMap;
+uniform sampler2D ambientOcclusionMap;
+
+uniform mat4 viewInverseMatrix;
+
+struct SurfaceOutputLux {
+	vec3 Albedo;
+	vec3 Normal;
+	vec3 Emission;
+	float Specular;
+	vec3 SpecularColor;
+	float Alpha;
+	float DeferredFresnel;
+};
+
+vec3 fixNormalSample(vec3 v)
+{
+  vec3 result = v - vec3(0.5f,0.5f,0.5f);
+  result.y = flipY ? -result.y : result.y;
+  return result;
+}
+
+vec3 normalVecOSToWS(vec3 normal)
+{
+  return normal;
+}
+
+#define OneOnLN2_x6 8.656170f
+#define M_PI 3.1415926535897932384626433832795f
+
+float CalcAtten(vec3 P, vec3 N, vec3 lightCentre, float lightRadius, float cutoff)
+{
+	//source: http://imdoingitwrong.wordpress.com/tag/glsl/
+    // calculate normalized light vector and distance to sphere light surface
+    float r = lightRadius;
+    vec3 L = lightCentre - P;
+    float distance = length(L);
+    float d = max(distance - r, 0.0f);
+    L /= distance;
+     
+    // calculate basic attenuation
+    float denom = d/r + 1.0f;
+    float attenuation = 1.0f / (denom*denom);
+     
+    // scale and bias attenuation such that:
+    //   attenuation == 0 at extent of max influence
+    //   attenuation == 1 when d == 0
+    attenuation = (attenuation - cutoff) / (1.0f - cutoff);
+    attenuation = max(attenuation, 0.0f);
+    
+    return attenuation;
+}
+
+vec4 LightingLuxPhongNDF (SurfaceOutputLux s, vec4 _LightColor0, vec3 lightDir, vec3 viewDir, float atten){
+  	// get base variables
+
+  	// normalizing lightDir makes fresnel smoother
+	lightDir = normalize(lightDir);
+
+	vec3 h = normalize (lightDir + viewDir);
+	// dotNL has to have max
+	float dotNL = max (0.0f, dot (s.Normal, lightDir));
+	float dotNH = max (0.0f, dot (s.Normal, h));
+	
+	// bring specPower into a range of 0.25 – 2048
+	float specPower = exp2(10.0f * s.Specular + 1) - 1.75f;
+
+	// Normalized Lighting Model:
+	// L = (c_diff * dotNL + F_Schlick(c_spec, l_c, h) * ( (spec + 2.0f)/8.0f) * dotNH˄spec * dotNL) * c_light
+	
+	// Specular: Phong lobe normal distribution function
+	//float spec = ((specPower + 2.0) * 0.125 ) * pow(dotNH, specPower) * dotNL; // would be the correct term
+	// we use late * dotNL to get rid of any artifacts on the backsides
+	float spec = specPower * 0.125f * pow(dotNH, specPower);
+
+	// Visibility: Schlick-Smith
+	float alpha = 2.0f / sqrt( 3.14159265f * (specPower + 2.0f) );
+	float visibility = 1.0f / ( (dotNL * (1.0f - alpha) + alpha) * ( dot(s.Normal, viewDir) * (1.0f - alpha) + alpha) ); 
+	spec *= visibility;
+	
+	// Fresnel: Schlick
+	// fixed3 fresnel = s.SpecularColor.rgb + ( 1.0f - s.SpecularColor.rgb) * pow(1.0f - saturate(dot(h, lightDir)), 5.0f);
+	// fast fresnel approximation:
+	vec3 fresnel = s.SpecularColor.rgb + ( 1.0f - s.SpecularColor.rgb) * exp2(-OneOnLN2_x6 * dot(h, lightDir));
+	// using dot(h, lightDir) or dot(h, viewDir) does not make any difference
+	//fixed3 fresnel = s.SpecularColor.rgb + ( 1.0f - s.SpecularColor.rgb) * exp2(-OneOnLN2_x6 * dot(h, viewDir));
+
+	// from here on we use fresnel instead of spec as it is fixed3 = color
+	fresnel *= spec;
+	
+	
+
+	// Final Composition
+	vec4 c;
+	// we only use fresnel here / and apply late dotNL
+	c.rgb = (s.Albedo.rgb * _LightColor0.rgb + _LightColor0.rgb * fresnel) * dotNL * (atten * 2.0f);
+		
+	c.a = s.Alpha + _LightColor0.a * fresnel * atten;
+	return c;
+}
+
+float saturate(float a)
+{
+	return max(0.0f,min(a,1.0f));
+}
+
+void main()
+{
+	vec3 cameraPosWS = viewInverseMatrix[3].xyz;
+	vec3 pointToLight0DirWS = normalize(Lamp0Pos - iFS_PointWS);
+	vec3 pointToLight1DirWS = normalize(Lamp1Pos - iFS_PointWS);
+	vec3 pointToCameraDirWS = normalize(cameraPosWS - iFS_PointWS);
+	vec3 normalOS = iFS_Normal;
+	vec3 tangentOS = iFS_Tangent;
+	vec3 binormalOS = iFS_Binormal;
+
+	// ------------------------------------------
+	vec3 cumulatedNormalOS = normalOS;
+
+	// ------------------------------------------
+	// Update UV
+	vec2 uv = iFS_UV * tiling;
+
+	// ------------------------------------------
+	// Add Normal from normalMap
+	vec3 normalTS = texture2D(normalMap,uv).xyz;
+	if (length(normalTS)<0.0001f)
+	{
+		cumulatedNormalOS = normalOS;
+	}
+	else
+	{
+		normalTS = fixNormalSample(normalTS);
+	    normalTS *= heightMapScale;
+		vec3 normalMapOS = normalTS.x*tangentOS + normalTS.y*binormalOS;
+		cumulatedNormalOS = cumulatedNormalOS + normalMapOS;
+		cumulatedNormalOS = normalize(cumulatedNormalOS);
+	}
+
+
+  	vec3 cumulatedNormalWS = normalVecOSToWS(cumulatedNormalOS);
+
+  	// ------------------------------------------
+  	// Compute Diffuse & Specular
+
+	SurfaceOutputLux o;
+	
+  	//float glossiness = texture2D(glossinessMap,uv).r * glossMult;
+  
+	vec4 diff_albedo = texture2D(diffuseMap, uv);
+	vec4 spec_albedo = texture2D(specularMap, uv);
+	// Diffuse Albedo
+	o.Albedo = diff_albedo.rgb;// * _Color.rgb;
+	o.Alpha = diff_albedo.a;// * _Color.a;
+	o.Normal = cumulatedNormalWS; //UnpackNormal(tex2D(_BumpMap, uv));
+	// Specular Color
+	o.SpecularColor = spec_albedo.rgb;
+	// Roughness – we just take it as it is and do not bring it into linear space (alpha!) so roughness textures must be authored using gamma values
+	o.Specular = spec_albedo.a;
+	
+	
+
+	// Light 0 contribution
+	vec4 contrib1 = vec4(0.0f,0.0f,0.0f,0.0f);
+	float atten1 = CalcAtten( iFS_PointWS, cumulatedNormalWS, Lamp0Pos, 8.0f, 20.0f);
+	contrib1 = LightingLuxPhongNDF(o,vec4(Lamp0Color * Lamp0Intensity * Lamp0Power,1.0f), pointToLight0DirWS, pointToCameraDirWS,atten1);//, diffContrib, specContrib,atten1);
+
+	// Light 1 contribution
+	vec4 contrib2 = vec4(0.0f,0.0f,0.0f,0.0f);
+	float atten2 = CalcAtten( iFS_PointWS, cumulatedNormalWS, Lamp1Pos, 8.0f, 20.0f);
+	contrib2 = LightingLuxPhongNDF(o,vec4(Lamp1Color * Lamp1Intensity * Lamp1Power,1.0f), pointToLight1DirWS, pointToCameraDirWS,atten2);//, diffContrib1, specContrib1,atten2);
+
+	// Lux IBL / ambient lighting
+	float Lux_HDR_Scale = HDR_Scale;
+	vec4 ExposureIBL;
+	ExposureIBL.x=DiffuseExposure;
+	ExposureIBL.y=SpecularExposure;
+
+	if (DiffHDR)
+	{
+		ExposureIBL.x *= pow(Lux_HDR_Scale,2.2333333f);
+	}
+	if (SpecHDR)
+	{
+		ExposureIBL.y *= pow(Lux_HDR_Scale,2.2333333f);
+	}
+
+
+	//	vec3 worldNormal = WorldNormalVector (IN, o.Normal);	
+	vec3 worldNormal = vec3(o.Normal.x,-o.Normal.y,o.Normal.z) ;
+	
+	//		add diffuse IBL
+	vec4	diff_ibl = textureCubeLod (environmentMap, worldNormal, DiffuseLOD);
+	diff_ibl = diff_ibl.bgra;
+	
+	if (LUX_LINEAR){
+			// if colorspace = linear alpha has to be brought to linear too (rgb already is): alpha = pow(alpha,2.233333333).
+			// approximation taken from http://chilliant.blogspot.de/2012/08/srgb-approximations-for-hlsl.html
+			diff_ibl.a *= diff_ibl.a * (diff_ibl.a * 0.305306011f + 0.682171111f) + 0.012522878f;
+	}
+	diff_ibl.rgb = diff_ibl.rgb * diff_ibl.a;
+	o.Emission = diff_ibl.rgb * ExposureIBL.x * o.Albedo;
+
+	//		add specular IBL		
+	//vec3 worldRefl = WorldReflectionVector (IN, o.Normal);
+	vec3 worldRefl = reflect(pointToCameraDirWS,cumulatedNormalWS);
+	vec4 spec_ibl = textureCubeLod (environmentMap, worldRefl, 6.0f - 8.0f * spec_albedo.a);
+	spec_ibl = spec_ibl.bgra;
+
+	if (LUX_LINEAR){
+		// if colorspace = linear alpha has to be brought to linear too (rgb already is): alpha = pow(alpha,2.233333333f).
+		// approximation taken from http://chilliant.blogspot.de/2012/08/srgb-approximations-for-hlsl.html
+		spec_ibl.a *= spec_ibl.a * (spec_ibl.a * 0.305306011f + 0.682171111f) + 0.012522878f;
+	}
+	
+	spec_ibl.rgb = spec_ibl.rgb * spec_ibl.a;
+	// fresnel based on spec_albedo.rgb and roughness (spec_albedo.a)
+	// taken from: http://seblagarde.wordpress.com/2011/08/17/hello-world/
+	// viewDir is in tangent-space (as we sample o.Normal) so we use o.Normal
+	vec3 FresnelSchlickWithRoughness = spec_albedo.rgb + ( max(spec_albedo.aaa,spec_albedo.rgb) - spec_albedo.rgb) * exp2(-OneOnLN2_x6 * saturate(dot(normalize(pointToCameraDirWS), o.Normal)));		
+	spec_ibl.rgb *= FresnelSchlickWithRoughness * ExposureIBL.y;
+	
+	// add diffuse and specular and conserve energy
+	o.Emission = (1.0f - spec_ibl.rgb) * o.Emission + spec_ibl.rgb;
+	
+	if (LUX_AO){
+		vec2 AO_uv = uv;
+		if (!LUX_AO_TILE)
+		{
+			AO_uv = iFS_UV;
+		}
+		vec4 ambientOcclusion = texture2D(ambientOcclusionMap, AO_uv);
+		o.Emission *= ambientOcclusion.a;
+	}
+   	vec4 Ambient_final = vec4(o.Albedo.rgb * ambientColor.rgb,1.0f);
+   
+   	vec4 emis_albedo = texture2D(emissiveMap, uv);
+	o.Emission += emis_albedo.rgb * emis_albedo.a;
+   
+  	// ------------------------------------------
+  	vec3 finalcolor = contrib1.rgb * contrib1.a  + contrib2.rgb * contrib2.a + (o.Emission + Ambient_final.rgb * Ambient_final.a);
+
+ 	// Final Color
+  	gl_FragColor =  vec4(finalcolor,1.0f);
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-Lux4SD
+Lux4SD - Unity Lux Open Source Shader port for Substance Designer 4
 ======
+
+Get Lux for Unity here:
+forum.unity3d.com/threads/221985-Lux-%E2%80%93-an-open-source-physically-based-shading-framework
+
